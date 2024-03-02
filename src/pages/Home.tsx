0685ba80ef54b5c8740c7f16473a58cc39e678af
Hla Htun <kshap.karki@gmail.com>
2024-03-01 20:31:47 -0800

    initial stylings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import "../styles/home.css"
+import logo from "../assets/logo.svg"
+import stardust from "../assets/stardust.svg"
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+      <>
+          <div className={"home-container"}>
+            Home
+          </div>
+          <div className="logo-container">
+              <img className={"stardust-l"} src={stardust} alt="Stardust" />
+              <img className="logo" src={logo} alt="Logo" />
+              <img className="stardust-r" src={stardust} alt="Stardust" />
+          </div>
+      </>
+  );
 };
 
 export default Home;
