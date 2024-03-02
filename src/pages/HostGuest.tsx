dac653580caf834a06277940a895aba1446d96c1
Hla Htun <kshap.karki@gmail.com>
2024-03-02 11:03:32 -0800

    images for host and player

--- src/pages/HostGuest.tsx
+++ src/pages/HostGuest.tsx
@@ -4,6 +4,9 @@ import RoleCard from '../components/RoleCard';
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import host from "../assets/host.webp"
+import player from "../assets/player.png"
+
 const HostGuest = () => {
   const [code, setCode] = useState<string>('');
 
@@ -18,56 +21,60 @@ const HostGuest = () => {
   };
 
   return (
-    <section className="bg-gradient-to-b from-hackathon-purple to-hackathon-gradient h-full w-full">
-      <div className="h-full w-full grid grid-rows-hackathon-row-2">
-        <div className="row-span-1 flex justify-center self-center">
-          <PinkBox className="px-14 py-4 text-2xl font-body italic font-semibold rounded-lg shadow-md">
-            You are a...
-          </PinkBox>
-        </div>
-        <div className="row-span-1 flex flex-row justify-center self-center h-full w-full z-50">
-          <div className="flex flex-row gap-12 h-4/5 items-center relative top-5 w-full justify-center">
-            <RoleCard className="rounded-lg shadow-lg h-full grid grid-rows-3 w-1/4 hover:scale-105 transition ease-in-out min-w-[316px] min-h-[517px]">
-              <div className="w-full bg-white rounded-tr-lg rounded-tl-lg">image</div>
-              <div className="self-center text-4xl italic font-semibold font-body cursor-default p-4 text-center">
-                The Host
-              </div>
-              <div className="px-10 pb-6 text-center text-lg font-body cursor-default select-none">
-                <div>Can direct the overall game</div>
-                <div>Can control the game flow</div>
-                <div>Can create your own monster</div>
-                <div>Cannot play the game</div>
-              </div>
-              <button
-                onClick={selectHost}
-                className="w-full bg-white py-4 rounded-br-lg rounded-bl-lg text-hackathon-black italic font-semibold text-2xl transition ease-in-out hover:text-white hover:bg-hackathon-black ">
-                SELECT
-              </button>
-            </RoleCard>
-            <RoleCard className="rounded-lg shadow-lg bg-hackathon-yellow text-hackathon-black h-full w-1/4 grid grid-rows-3 hover:scale-105 transition ease-in-out min-w-[316px] min-h-[517px]">
-              <div className="w-full bg-white rounded-tr-lg rounded-tl-lg">image</div>
-              <div className="self-center text-4xl italic font-semibold font-body cursor-default p-4 text-center">
-                The Player
-              </div>
-              <div className="px-10 pb-6 text-center text-lg font-medium font-body cursor-default select-none">
-                <div>Only can play the game</div>
-                <div className="pb-2">Beat the monster & Become a hero!</div>
-                <input
-                  type="text"
-                  placeholder="Type your invitation code"
-                  className="rounded-md py-2 px-3 w-full font-normal shadow-md"></input>
-              </div>
-              <button
-                onClick={selectGuest}
-                className="w-full bg-hackathon-black py-4 rounded-br-lg rounded-bl-lg text-white italic font-semibold text-2xl relative bottom-0 hover:text-hackathon-black hover:bg-white transition ease-in-out">
-                SELECT
-              </button>
-            </RoleCard>
+      <section className="bg-gradient-to-b from-hackathon-purple to-hackathon-gradient h-full w-full">
+        <div className="h-full w-full grid grid-rows-hackathon-row-2">
+          <div className="row-span-1 flex justify-center self-center">
+            <PinkBox className="px-14 py-4 text-2xl font-body italic font-semibold rounded-lg shadow-md">
+              You are a...
+            </PinkBox>
+          </div>
+          <div className="row-span-1 flex flex-row justify-center self-center h-full w-full z-50">
+            <div className="flex flex-row gap-12 h-4/5 items-center relative top-5 w-full justify-center">
+              <RoleCard className="rounded-lg shadow-lg h-full grid grid-rows-3 w-1/4 hover:scale-105 transition ease-in-out min-w-[316px] max-h-[650px]">
+                <div className="w-full bg-white rounded-tr-lg rounded-tl-lg">
+                  <img src={host} alt="host" className="w-full h-full object-cover custom-object-position" />
+                </div>
+                <div className="self-center text-4xl italic font-semibold font-body cursor-default p-4 text-center">
+                  The Host
+                </div>
+                <div className="px-10 pb-6 text-center text-lg font-body cursor-default select-none">
+                  <div>Can direct the overall game</div>
+                  <div>Can control the game flow</div>
+                  <div>Can create your own monster</div>
+                  <div>Cannot play the game</div>
+                </div>
+                <button
+                    onClick={selectHost}
+                    className="w-full bg-white py-4 rounded-br-lg rounded-bl-lg text-hackathon-black italic font-semibold text-2xl transition ease-in-out hover:text-white hover:bg-hackathon-black ">
+                  SELECT
+                </button>
+              </RoleCard>
+              <RoleCard className="rounded-lg shadow-lg bg-hackathon-yellow text-hackathon-black h-full w-1/4 grid grid-rows-3 hover:scale-105 transition ease-in-out min-w-[316px] max-h-[650px]">
+                <div className="w-full bg-white rounded-tr-lg rounded-tl-lg">
+                  <img src={player} alt="host" className="w-full h-full object-cover custom-object-position" />
+                </div>
+                <div className="self-center text-4xl italic font-semibold font-body cursor-default p-4 text-center">
+                  The Player
+                </div>
+                <div className="px-10 pb-6 text-center text-lg font-medium font-body cursor-default select-none">
+                  <div>Only can play the game</div>
+                  <div className="pb-2">Beat the monster & Become a hero!</div>
+                  <input
+                      type="text"
+                      placeholder="Type your invitation code"
+                      className="rounded-md py-2 px-3 w-full font-normal shadow-md"></input>
+                </div>
+                <button
+                    onClick={selectGuest}
+                    className="w-full bg-hackathon-black py-4 rounded-br-lg rounded-bl-lg text-white italic font-semibold text-2xl relative bottom-0 hover:text-hackathon-black hover:bg-white transition ease-in-out">
+                  SELECT
+                </button>
+              </RoleCard>
+            </div>
           </div>
         </div>
-      </div>
-      <BlueWave />
-    </section>
+        <BlueWave />
+      </section>
   );
 };
 
