a4a3aa311a9e52552140cf0c4ec3e009a45ffd84
Derek R Williams <derekofficial00@gmail.com>
2024-03-02 20:26:42 -0800

    getting images

---
+++ convex/https.ts
@@ -0,0 +1,55 @@
+import { httpRouter } from 'convex/server';
+import { query, httpAction } from './_generated/server';
+import { v } from 'convex/values';
+
+const http = httpRouter();
+
+export const getImageId = query({
+    args : { gameId : v.number() },
+    handler : async (ctx, args) => {
+        let game = await ctx.db
+            .query("games")
+            .filter((q) => q.eq(q.field("id"), args.gameId))
+            .first();
+        
+        let imageStorageId = game?.imageId;
+
+        if (!imageStorageId) {
+            // TODO: default image
+        }
+
+        return imageStorageId;
+    }
+});
+
+export const getImageURL = query({
+    args : { gameId : v.number() },
+    handler : async (ctx, args) => {
+        let game = await ctx.db
+            .query("games")
+            .filter((q) => q.eq(q.field("id"), args.gameId))
+            .first();
+        
+        let imageStorageId = game?.imageId;
+
+        return await ctx.storage.getUrl(imageStorageId);
+    }
+});
+
+/*
+http.route({
+    path : "/image/:imageId",
+    method : "GET",
+    handler : httpAction(async (ctx, req) => {
+        const { searchParams } = new URL(req.url);
+        let storageId = searchParams.get("storageId");
+
+        if(!storageId) {
+            return { status: 404, body: "Imabe Not found" };
+        }
+
+        const blob = await ctx.storage.get(storageId);
+        return { status: 200, body: blob};
+    }
+});
+*/
--- convex/schema.ts
+++ convex/schema.ts
@@ -18,7 +18,7 @@ export default defineSchema({
   games: defineTable({
     id: v.number(),
     status: v.string(),
-    imageId: v.optional(v.string()),
+    imageId: v.optional(v.id("_storage")),
     settingDescription: v.string(),
     monsterDescription: v.string(),
     monsterHealthPoints: v.number(),
